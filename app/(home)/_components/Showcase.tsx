327271a70760122ab44b8ca90c4af254b2a88423
Rizky Ramadhan <ramarizdev@gmail.com>
2024-05-13 10:22:30 +0700

    feat(showcase): add section title

--- app/(home)/_components/Showcase.tsx
+++ app/(home)/_components/Showcase.tsx
@@ -3,6 +3,9 @@ import ProjectGrid from '@/components/ProjectGrid';
 const Showcase = () => {
   return (
     <section className='mb-24'>
+      <h2 className='text-5xl font-roboto-serif font-semibold text-center leading-tight mb-16'>
+        Recent Showcase
+      </h2>
       <ProjectGrid />
     </section>
   );
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Services />
       <AboutUs />
       <Process />
-      <Testimonials />
       <Showcase />
+      <Testimonials />
     </main>
   );
 }
